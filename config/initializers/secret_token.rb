fe40e8c6ddd252abc3a8e9a9eab38f5628fbb479
son <son@lincolnix.net>
2014-08-20 15:18:54 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MattJones::Application.config.secret_key_base = '863767027250d55f2e77e4359b758e43a763af26cfbb3cdb4c9a8f22dbce1f4efa09a66310810a0defe289e13a421318d99ad19596563807f57293b584186d9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MattJones::Application.config.session_store :cookie_store, key: '_MattJones_session'
