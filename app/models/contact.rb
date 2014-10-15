1a8f1de86b869cb7c60aa07ff31bfb53f27b31ac
son <son@lincolnix.net>
2014-10-15 13:59:49 +0000

    minor

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -11,8 +11,6 @@ class Contact < MailForm::Base
     {
       :subject => "Nebraska Swoop",
       :to => "jayhopheli@gmail.com",
-      #:to => "anderson.reinkordt@gmail.com",
-      #:bcc => ["andornotweb@gmail.com"],
       :from => %("#{name}" <#{email}>),
       :'reply_to' => %(#{email})
     }
